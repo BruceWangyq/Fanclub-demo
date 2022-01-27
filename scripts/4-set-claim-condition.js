bb8383b067893420334ae1f6c8b2dae8fb7732f8
BruceWangyq <brucewangyq@gmail.com>
2022-01-27 16:56:05 -0500

    feat: update the allowList

--- scripts/4-set-claim-condition.js
+++ scripts/4-set-claim-condition.js
@@ -1,20 +1,28 @@
 import sdk from "./1-initialize-sdk.js";
 
 const bundleDrop = sdk.getBundleDropModule(
-  "0xB6edF6E4e51A3D8c1a40e80e0a25E561d0Df653b"
+  "0x9a4c13d336D85EF571E856803bb702BC108E12eD"
 );
 
 (async () => {
   try {
-    const claimConditionFactory = bundleDrop.getClaimConditionFactory();
+    const factory = bundleDrop.getClaimConditionFactory();
+
     // Specify conditions.
-    claimConditionFactory.newClaimPhase({
+    const allowList = [
+      "0xB78d0Ba89a5a01F8FC09E896B841be05cc247393",
+      "0x07C331E48d7767062D44C1F0a64566859AA265Fc",
+      "0x5E0c9C286a993f82D7B5536f5EF6899d7F42dDA2",
+    ];
+    const claimPhase = factory.newClaimPhase({
       startTime: new Date(),
-      maxQuantity: 50_000,
+      maxQuantity: 10,
       maxQuantityPerTransaction: 1,
     });
 
-    await bundleDrop.setClaimCondition(0, claimConditionFactory);
+    claimPhase.setSnapshot(allowList);
+
+    await bundleDrop.setClaimCondition(0, factory);
     console.log(
       "✅ Successfully set claim condition on bundle drop:",
       bundleDrop.address
